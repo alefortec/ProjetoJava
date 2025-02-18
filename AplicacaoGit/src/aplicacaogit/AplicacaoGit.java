a24199672ae492a58471fa3e5b61762c7ef49044
Ale Lima <alessandro@fortecpraiagrande.com.br>
2025-02-18 08:09:54 -0300

    Adicionado a função de soma

--- AplicacaoGit/src/aplicacaogit/AplicacaoGit.java
+++ AplicacaoGit/src/aplicacaogit/AplicacaoGit.java
@@ -1,10 +1,16 @@
 package aplicacaogit;
-
+import java.util.Scanner;
 public class AplicacaoGit {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Meu primeiro software "
         + "versionado com git e hospedado no GitHub!");
+        System.out.println("Digite o primeiro valor:");
+        int num1 = scan.nextInt();
+        System.out.println("Digite o segundo valor:");
+        int num2 = scan.nextInt();
+        System.out.println("A Soma é: "+(num1+num2));
     }
     
 }
