44671ea8814e5957824225fbe1085f307c46d0c7
Ale Lima <alessandro@fortecpraiagrande.com.br>
2025-02-18 07:56:56 -0300

    Primeiro Commit

---
+++ AplicacaoGit/src/aplicacaogit/AplicacaoGit.java
@@ -0,0 +1,10 @@
+package aplicacaogit;
+
+public class AplicacaoGit {
+
+    public static void main(String[] args) {
+        System.out.println("Meu primeiro software "
+        + "versionado com git e hospedado no GitHub!");
+    }
+    
+}
